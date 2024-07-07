6fa9ec35715d02080931e6b40e24bcf877dfc2b5
olga <uvbookshelf@gmail.com>
2024-07-08 00:21:16 +0300

    «added_test_scenario_of_checkout»

--- testplane-tests/api.testplane.ts
+++ testplane-tests/api.testplane.ts
@@ -29,4 +29,38 @@ describe("Api", () => {
         expect(data_price[0]).toBe(data_card_price[0]);
         expect(data_name[0]).toBe(data_card_name[0]);
     });
+
+    it('Проверка сценария заказа товара и получения сообщения об успешности и номера заказа из не более 6 цифр', async({browser}) => {
+        const regex = new RegExp(/^(\d){1,6}$/g);
+        await browser.url('http://localhost:3000/hw/store/catalog/0');
+        const button_for_add = await Promise.all(
+            await browser.$$('.ProductDetails').map((card) => 
+                card.$('.ProductDetails-AddToCart')
+        ));
+        await button_for_add[0].click();
+        await browser.url('http://localhost:3000/hw/store/cart');
+        const button_for_checkout = await Promise.all(
+            await browser.$$('.Form').map((button) => 
+                button.$('.Form-Submit')
+        ));
+        console.log(button_for_checkout[0]);
+        await browser.$('.Form-Field_type_name').setValue('my name');
+        await browser.$('.Form-Field_type_phone').setValue('9099999999');
+        await browser.$('.Form-Field_type_address').setValue('address');
+
+        await button_for_checkout[0].click();
+        let temp_message = await Promise.all(
+            await browser.$$('.Cart').map((button) => 
+                button.$('.alert-heading').getText()
+        ));
+        let temp_order = await Promise.all(
+            await browser.$$('.Cart').map((button) => 
+                button.$('.Cart-Number').getText()
+        ));
+
+        console.log('order:',temp_order[0]);
+        await expect(temp_message[0]).toBe('Well done!');
+        await expect(regex.test(temp_order[0])).toBe(true);
+
+    });
 });
